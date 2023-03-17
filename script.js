1c72af4dd7a09df22ceb114b2be5576599b0a808
bharathgoud77 <92040880+bharathgoud77@users.noreply.github.com>
2023-03-17 19:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const person = {
     };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players;
+const team1 = [...players]; // Using the spread operator to create a new copy of the array
+const cap1 = { ...person }; // Using the spread operator to create a new copy of the object
 
 
 export {players,person,team,team1,cap1}
